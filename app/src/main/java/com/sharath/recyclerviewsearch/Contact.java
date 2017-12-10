f89a159bb03b8c5328f134d00fc3a2983375e0ad
YakseeHyd <suresh@yaksee.com>
2017-12-10 16:30:15 +0530

    New Contact class is added

---
+++ app/src/main/java/com/sharath/recyclerviewsearch/Contact.java
@@ -0,0 +1,28 @@
+package com.sharath.recyclerviewsearch;
+
+/**
+ * Created by yaksee on 12/10/17.
+ */
+
+public class Contact {
+
+    String name;
+    String image;
+    String phone;
+
+    public Contact() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
