0e4066d9f783b03d81d40e8ecde98c94c5431296
YakseeHyd <suresh@yaksee.com>
2017-12-10 17:27:49 +0530

    New adapter class ContactAdapter is added.

---
+++ app/src/main/java/com/sharath/recyclerviewsearch/ContactAdapter.java
@@ -0,0 +1,98 @@
+package com.sharath.recyclerviewsearch;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.List;
+
+/**
+ * Created by yaksee on 12/10/17.
+ */
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.MyViewHolder> implements Filterable {
+
+    private Context context;
+    private List<Contact> contactList;
+    private List<Contact> contactListFiltered;
+    private ContactsAdapterListener listener;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView name, phone;
+        public ImageView thumbnail;
+
+        public MyViewHolder(View view) {
+            super(view);
+            name = view.findViewById(R.id.name);
+            phone = view.findViewById(R.id.phone);
+            thumbnail = view.findViewById(R.id.thumbnail);
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onContactSelected(contactListFiltered.get(getAdapterPosition()));
+                }
+            });
+        }
+    }
+
+    public ContactAdapter(Context context, List<Contact> contactList, ContactsAdapterListener listener) {
+        this.context = context;
+        this.contactList = contactList;
+        this.listener = listener;
+        this.contactListFiltered = contactList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.user_row_item, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        final  Contact contact = contactListFiltered.get(position);
+        holder.name.setText(contact.getName());
+        holder.phone.setText(contact.getPhone());
+
+        Glide.with(context)
+                .load(contact.getImage())
+                .apply(RequestOptions.circleCropTransform())
+                .into(holder.thumbnail);
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactListFiltered.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                return null;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+
+            }
+        };
+    }
+
+    public interface ContactsAdapterListener {
+        void onContactSelected(Contact contact);
+    }
+}
